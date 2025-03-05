fa3f27a1cd51b3790aff4059a19dff9aeb9ecae4
Anton Tomashevskiy <anton.tomashevskiy@yandex.ru>
2025-03-05 17:21:27 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+// webpack.config.js
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+                publicPath: ''// переписали точку выхода, используя утилиту path
+    },
+    mode: 'development', 
+    devServer: {
+        static: path.resolve(__dirname, './dist'), 
+        compress: true, 
+        port: 8080, 
+        open: true 
+      },
+      module: {
+        rules: [ 
+          {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: '/node_modules/'
+          },
+          {
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
+          },
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+  'postcss-loader']
+          },
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+      ]
+}
+
